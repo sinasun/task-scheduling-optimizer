65d29f6ae87ba3b85eff92a3fdcb351ee78b4f46
sinasun <sinakhodaveisi@gmail.com>
2023-11-03 18:14:15 -0400

    Updated: update unscheduled tasks and loop until it doesn't change

--- src/scheduling/algorithm/PriorityDueDateSchedulingAlgorithm.java
+++ src/scheduling/algorithm/PriorityDueDateSchedulingAlgorithm.java
@@ -14,32 +14,46 @@ public class PriorityDueDateSchedulingAlgorithm implements SchedulingAlgorithm {
 	public PriorityDueDateSchedulingAlgorithm(Scheduler scheduler) {
 		this.scheduler = scheduler;
 	}
-
 	@Override
 	public void applyAlgorithm() {
 		ArrayList<Task> unscheduledTasks = scheduler.getUnscheduledTasks(scheduler.getTasks());
+		int previousUnscheduledTaskCount = unscheduledTasks.size();
+		int unchangedTaskCount = 0;
+		int maxUnchangedTaskCount = 1; 
 
-		// Sort unscheduled tasks by importance (Priority / Due Date) in descending order
-		Collections.sort(unscheduledTasks, new Comparator<Task>() {
-			@Override
-			public int compare(Task task1, Task task2) {
-				double importance1 = (double) task1.getPriorityLevel() / task1.getDueDate();
-				double importance2 = (double) task2.getPriorityLevel() / task2.getDueDate();
-				return Double.compare(importance2, importance1);
-			}
-		});
+		while (!unscheduledTasks.isEmpty() && unchangedTaskCount <= maxUnchangedTaskCount) {
+			// Sort unscheduled tasks by importance (Priority / Due Date) in descending order
+			Collections.sort(unscheduledTasks, new Comparator<Task>() {
+				@Override
+				public int compare(Task task1, Task task2) {
+					double importance1 = (double) task1.getPriorityLevel() / task1.getDueDate();
+					double importance2 = (double) task2.getPriorityLevel() / task2.getDueDate();
+					return Double.compare(importance2, importance1);
+				}
+			});
 
-		for (Task task : unscheduledTasks) {
-			if (task.getTaskDependencies().isEmpty()) {
-				// Find the first available employee for the job type
-				Employee employee = scheduler.findFirstAvailableEmployeeForJobType(task.getEmployeeType());
+			for (Task task : unscheduledTasks) {
+				if (task.getTaskDependencies().isEmpty()) {
+					// Find the first available employee for the job type
+					Employee employee = scheduler.findFirstAvailableEmployeeForJobType(task.getEmployeeType());
 
-				if (employee != null) {
-					// Allocate the task to the employee with the highest importance
-					scheduler.allocateTaskToEmployee(task, employee, employee.getFirstFreeDay(, task.getProductionTime());
+					if (employee != null) {
+						// Allocate the task to the employee with the highest importance
+						scheduler.allocateTaskToEmployee(task, employee, 0, task.getProductionTime());
+					}
 				}
 			}
+
+			// Update the list of unscheduled tasks after allocation
+			ArrayList<Task> newUnscheduledTasks = scheduler.getUnscheduledTasks(scheduler.getTasks());
+
+			if (newUnscheduledTasks.size() == previousUnscheduledTaskCount) {
+				unchangedTaskCount++;
+			} else {
+				unchangedTaskCount = 0;
+			}
+
+			previousUnscheduledTaskCount = newUnscheduledTasks.size();
 		}
 	}
 }
-
